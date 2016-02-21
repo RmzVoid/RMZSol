fb87e68d306534be507e5e030b488baf7a98767c
rmzvoid <rmzvoid@ya.ru>
2016-02-22 01:21:27 +0300

    Ordered build configurations Add filter creation to pass to callout

--- RMZControl/RMZControl.cpp
+++ RMZControl/RMZControl.cpp
@@ -8,39 +8,102 @@ BOOL CheckError(DWORD status, LPCWSTR message);
 
 int main()
 {
-	//
-	// Create session
-	//
-	FWPM_SESSION session = { 0 };
-	session.displayData.name = L"RMZControl session";
-	session.displayData.description = L"RMZControl session description";
-	session.flags = FWPM_SESSION_FLAG_DYNAMIC;
-
 	HANDLE engine;
-
-	DWORD status = FwpmEngineOpen(NULL, RPC_C_AUTHN_DEFAULT, NULL, &session, &engine);
-
-	if (!CheckError(status, L"FwpmEngineOpen"))
-		return status;
-	
-	// Add provider
+	FWPM_SESSION session = { 0 };
 	FWPM_PROVIDER provider = { 0 };
-	provider.displayData.name = L"RMZControl provider";
-	provider.displayData.description = L"RMZControl provider description";
-	provider.providerKey = rmzProviderGuid;
-
-	status = FwpmProviderAdd(engine, &provider, NULL);
-
-	if (!CheckError(status, L"FwpmProviderAdd")) goto exit;
-
-	//
-	// Add sublayer
-	//
 	FWPM_SUBLAYER sublayer = { 0 };
-	sublayer.displayData.name = L"RMZControl sublayer";
-	sublayer.displayData.description = L"RMZControl sublayer description";
-exit:
+	FWPM_CALLOUT callout = { 0 };
+	FWPM_FILTER filter = { 0 };
 
+	try
+	{
+		//
+		// Create session
+		//
+		session.displayData.name = L"RMZControl session";
+		session.displayData.description = L"RMZControl session description";
+		session.flags = FWPM_SESSION_FLAG_DYNAMIC;
+
+		DWORD status = FwpmEngineOpen(NULL, RPC_C_AUTHN_DEFAULT, NULL, &session, &engine);
+
+		if (!CheckError(status, L"FwpmEngineOpen"))
+			return status;
+
+		// Add provider
+		provider.displayData.name = L"RMZControl provider";
+		provider.displayData.description = L"RMZControl provider description";
+		provider.providerKey = rmzProviderGuid;
+
+		status = FwpmProviderAdd(engine, &provider, NULL);
+
+		if (!CheckError(status, L"FwpmProviderAdd")) throw status;
+
+		//
+		// Add sublayer
+		//
+		sublayer.displayData.name = L"RMZControl sublayer";
+		sublayer.displayData.description = L"RMZControl sublayer description";
+		sublayer.subLayerKey = rmzSublayerGuid;
+		sublayer.providerKey = (GUID*)&rmzProviderGuid;
+		sublayer.weight = 0x01;
+
+		status = FwpmSubLayerAdd(engine, &sublayer, NULL);
+
+		if (!CheckError(status, L"FwpmSublayerAdd")) throw status;
+
+		//
+		// Add callout
+		//
+		callout.displayData.name = L"RMZControl callout";
+		callout.displayData.description = L"RMZControl callout description";
+		callout.calloutKey = rmzCalloutGuid;
+		callout.providerKey = (GUID*)&rmzProviderGuid;
+		callout.applicableLayer = FWPM_LAYER_ALE_AUTH_CONNECT_V4;
+
+		status = FwpmCalloutAdd(engine, &callout, NULL, &callout.calloutId);
+
+		if (!CheckError(status, L"FwpmCalloutAdd")) throw status;
+
+		//
+		// Add filter
+		//
+		const int numConditions = 2;
+		FWPM_FILTER_CONDITION condition[numConditions] = { { 0 }, { 0 } };
+		condition[0].fieldKey = FWPM_CONDITION_IP_PROTOCOL;
+		condition[0].matchType = FWP_MATCH_EQUAL;
+		condition[0].conditionValue.type = FWP_UINT8;
+		condition[0].conditionValue.uint8 = IPPROTO_TCP;
+
+		condition[1].fieldKey = FWPM_CONDITION_IP_REMOTE_PORT;
+		condition[1].matchType = FWP_MATCH_EQUAL;
+		condition[1].conditionValue.type = FWP_UINT16;
+		condition[1].conditionValue.uint16 = 7777;
+
+		filter.displayData.name = L"RMZControl filter";
+		filter.displayData.description = L"RMZControl filter description";
+		filter.filterKey = rmzFilterGuid;
+		filter.numFilterConditions = numConditions;
+		filter.filterCondition = condition;
+		filter.providerKey = (GUID*)&rmzProviderGuid;
+		filter.layerKey = FWPM_LAYER_ALE_AUTH_CONNECT_V4;
+		filter.subLayerKey = rmzSublayerGuid;
+		filter.weight.type = FWP_EMPTY;
+		filter.action.type = FWP_ACTION_CALLOUT_UNKNOWN;
+		filter.action.calloutKey = rmzCalloutGuid;
+
+		status = FwpmFilterAdd(engine, &filter, NULL, &filter.filterId);
+
+		if (!CheckError(status, L"FwpmFilterAdd")) throw status;
+
+	}
+	catch (DWORD status)
+	{
+		wprintf(L"Catched exception %0xx\r\n", status);
+	}
+
+	FwpmFilterDeleteByKey(engine, &rmzFilterGuid);
+	FwpmCalloutDeleteByKey(engine, &rmzCalloutGuid);
+	FwpmSubLayerDeleteByKey(engine, &rmzSublayerGuid);
 	FwpmProviderDeleteByKey(engine, &rmzProviderGuid);
 	FwpmEngineClose(engine);
 
@@ -51,12 +114,12 @@ BOOL CheckError(DWORD status, LPCWSTR message)
 {
 	if (status != ERROR_SUCCESS)
 	{
-		wprintf(L"%s failed: %d", message, status);
+		wprintf(L"%s failed: %d\r\n", message, status);
 		return FALSE;
 	}
 	else
 	{
-		wprintf(L"%s success", message);
+		wprintf(L"%s success\r\n", message);
 		return TRUE;
 	}
 }
